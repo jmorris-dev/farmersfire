7578c0266e2b62e9a5ceb7632e07da76bdab426e
Joshua Morris <25271173+jmorris1990@users.noreply.github.com>
2019-12-19 18:47:59 -0500

    silly

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def check():
-    return "hello munchkin"
+    return "you've just been gnomed!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
